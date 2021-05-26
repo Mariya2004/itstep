8d4341a3f1f1b1ddf4820b6f8c35ea78d11c3e17
Mariya2004 <mariya.shapar@gmail.com>
2021-05-26 13:29:41 +0300

    task7homework

---
+++ task4/main.js
@@ -0,0 +1,27 @@
+const something = 'y'
+
+const x = {
+
+something: something
+
+}
+
+
+function dosomething(foo) {
+
+    return foo * 2
+    
+    }
+    
+    const dosomething = function(foo) {
+    
+    return foo * 2
+    
+    }
+
+
+    const foo1 = () => {
+
+        //...
+        
+        }
